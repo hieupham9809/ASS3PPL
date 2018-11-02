986381bcc25d216613a4652cc031c1c2ab7a6886
hieupham <hieupham9809@gmail.com>
2018-11-03 03:01:21 +0700

    id undeclared

--- src/main/mp/checker/StaticCheck.py
+++ src/main/mp/checker/StaticCheck.py
@@ -17,26 +17,45 @@ class Symbol:
         self.name = name
         self.mtype = mtype
         self.value = value
-def check_redeclared(has_decl, decl):
+def check_redeclared(has_decl, decl, k):
     temp_name = ""
-    if type(decl) is VarDecl:
+    #if type(decl) is VarDecl:
+    if k == "variable":
         temp_name = decl.variable.name
         #if Utils.lookup(temp_name, has_decl,lambda x: x.name):
-        if any((temp_name != hde.name ) for hde in has_decl):
+        if any((temp_name == hde.name ) for hde in has_decl):
             raise Redeclared(Variable(), temp_name)
         else: return Symbol(temp_name,decl.varType)
-    else: 
+    elif k == "procedure": 
         temp_name = decl.name.name
         #if Utils.lookup(temp_name, has_decl,lambda x: x.name):
-        if any((temp_name != hde.name ) for hde in has_decl):
+        if any((temp_name == hde.name ) for hde in has_decl):
             if type(decl.returnType) is VoidType:
                 raise Redeclared(Procedure(), temp_name)
             else: raise Redeclared(Function(), temp_name)
         else: return Symbol(temp_name,MType([x.varType for x in decl.param],decl.returnType))
-            
+    else:
+        temp_name = decl.variable.name
+        #if Utils.lookup(temp_name, has_decl,lambda x: x.name):
+        if any((temp_name == hde.name ) for hde in has_decl):
+            raise Redeclared(Parameter(), temp_name)
+        else: return Symbol(temp_name,decl.varType)    
 
     
-    
+def check_operator(left, right):
+    #print(type(left))
+    #print(type(right))
+    # if (type(left), type(right)) == (IntType, IntType):
+    #     return IntType()
+    # elif (type(left),type(right)) in [(IntType, FloatType), (FloatType, IntType), (FloatType, FloatType)]:
+    #     return FloatType()
+    # else: return None
+
+    if (left,right) == (IntType, IntType):
+        return IntType()
+    elif (left,right) in [(IntType, FloatType), (FloatType, IntType), (FloatType, FloatType)]:
+        return FloatType()
+    else: return None
     
 
 class StaticChecker(BaseVisitor,Utils):
@@ -64,27 +83,124 @@ class StaticChecker(BaseVisitor,Utils):
     def visitProgram(self,ast, c): 
         has_decl = c.copy()
         for decl in ast.decl:
-            has_decl.append(check_redeclared(has_decl,decl))
+            has_decl.append(check_redeclared(has_decl,decl, "variable" if type(decl) is VarDecl else "procedure"))
         #return [self.visit(x,c) for x in ast.decl]
         for decl in ast.decl:
             self.visit(decl, has_decl)
         return None
 
-    def visitFuncDecl(self,ast, c): 
-        return list(map(lambda x: self.visit(x,(c,True)),ast.body)) 
-    
+    def visitFuncDecl(self,ast, c):
+        globaldecl = c.copy()
+        param_sub_scope = []
+        local_sub_scope = []
+        for vardecl in ast.param:
+            param_sub_scope.append(check_redeclared(param_sub_scope, vardecl,"parameter"))
+        local_sub_scope += param_sub_scope
+        for vardecl in ast.local:
+            local_sub_scope.append(check_redeclared(local_sub_scope, vardecl,"variable"))
+        
+        for i in range(len(globaldecl)):
+            for j in range(len(local_sub_scope)):
+                if globaldecl[i].name == local_sub_scope[j].name:
+                    globaldecl[i] = local_sub_scope[j]
+        #return list(map(lambda x: self.visit(x,c),ast.body)) 
+        return [self.visit(x, globaldecl) for x in ast.body]
 
     def visitCallStmt(self, ast, c): 
-        at = [self.visit(x,(c[0],False)) for x in ast.param]
+        at = [self.visit(x,c) for x in ast.param]
         
-        res = self.lookup(ast.method.name,c[0],lambda x: x.name)
+        res = self.lookup(ast.method.name,c,lambda x: x.name)
         if res is None or not type(res.mtype) is MType or not type(res.mtype.rettype) is VoidType:
             raise Undeclared(Procedure(),ast.method.name)
         elif len(res.mtype.partype) != len(at):
             raise TypeMismatchInStatement(ast)            
         else:
+            match_type = zip(res.mtype.partype, at)
+            for mt0,mt1 in match_type:
+                if type(mt0) != type(mt1):
+                    
+                    if not(type(mt0), type(mt1)) == (FloatType, IntType):
+                    #if type(mt0) != FloatType or type(mt1) != IntType:  
+                    #if not isinstance(mt0,FloatType) or not isinstance(mt1,IntType):
+                        raise TypeMismatchInStatement(ast)
+                    else: pass
+                    
             return res.mtype.rettype
 
+    def visitBinaryOp(self, ast, c):
+        
+        left = self.visit(ast.left, c)
+        print(left)
+        right = self.visit(ast.right, c)
+        check = check_operator(left, right)
+        if ast.op in ['+','-','*']:
+            if check:
+                return check
+            else: raise TypeMismatchInExpression(ast)
+        elif ast.op == '/':
+            if check:
+                return FloatType()
+            else: raise TypeMismatchInExpression(ast)
+        elif ast.op.lower() in ['div','mod']:
+            if type(check) is IntType:
+                return check 
+            else: raise TypeMismatchInExpression(ast)  
+        elif ast.op.lower() in ['or','and','andthen','orelse']:
+            if (type(left), type(right)) == (BoolType, BoolType):
+                return BoolType()
+            else: raise TypeMismatchInExpression(ast)
+        else:
+            if check:
+                print("check")
+                return BoolType()
+            else: raise TypeMismatchInExpression(ast)
+    
+    def visitCallExpr(self,ast,c):
+        at = [self.visit(x,c) for x in ast.param]
+        
+        res = self.lookup(ast.method.name,c,lambda x: x.name)
+        if res is None or not type(res.mtype) is MType or type(res.mtype.rettype) is VoidType:
+            raise Undeclared(Function(),ast.method.name)
+        elif len(res.mtype.partype) != len(at):
+            raise TypeMismatchInStatement(ast)            
+        else:
+            match_type = zip(res.mtype.partype, at)
+            for mt0,mt1 in match_type:
+                if type(mt0) != type(mt1):
+                    
+                    if not(type(mt0), type(mt1)) == (FloatType, IntType):
+                    #if type(mt0) != FloatType or type(mt1) != IntType:  
+                    #if not isinstance(mt0,FloatType) or not isinstance(mt1,IntType):
+                    
+                        raise TypeMismatchInStatement(ast)
+                    else: pass
+                    
+            return res.mtype.rettype
+    def visitUnaryOp(self, ast, c):
+        expr = self.visit(ast.body, c)
+        if ast.op == '-':
+            if type(expr) == IntType:
+                return IntType()
+            elif type(expr) == FloatType:
+                return FloatType()
+            else: raise TypeMismatchInExpression(ast)
+        else:           # ast.op == 'not':
+            if type(expr) == BoolType:
+                return BoolType()
+            else: raise TypeMismatchInExpression(ast)
+    def visitId(self, ast, c):
+        declared_id = self.lookup(ast.name, c, lambda x: x.name)
+        if declared_id is None or declared_id.mtype is MType :
+            raise Undeclared(Identifier(),ast.name)
+        else:
+            print(declared_id.mtype)
+            return declared_id.mtype
+
+    
     def visitIntLiteral(self,ast, c): 
         return IntType()
+    def visitFloatLiteral(self,ast,c):
+        return FloatType()
+    def visitBooleanLiteral(self,ast,c):
+        return BoolType()
 
--- src/test/CheckerSuite.py
+++ src/test/CheckerSuite.py
@@ -3,7 +3,7 @@ from TestUtils import TestChecker
 from AST import *
 
 class CheckerSuite(unittest.TestCase):
-    def test_redeclared_builtin_procedure(self):
+    '''def test_redeclared_builtin_procedure(self):
         """Simple program: int main() {} """
         input = Program([
                 FuncDecl(Id("putIntLn"),[],[],[])])
@@ -25,7 +25,86 @@ class CheckerSuite(unittest.TestCase):
         expect = "Redeclared Procedure: foo"
         self.assertTrue(TestChecker.test(input,expect,402))
 
-    def test_diff_numofparam_stmt(self):
+    def test_redeclared_param_vs_local(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[VarDecl(Id("a"),IntType)],[VarDecl(Id("a"),IntType)],[])])
+        expect = "Redeclared Variable: a"
+        self.assertTrue(TestChecker.test(input,expect,403))
+
+    def test_redeclared_param(self):
+        """Simple program: int main() {} """
+        input = Program([
+                VarDecl(Id("name"),FloatType),
+                FuncDecl(Id("foo2"),[VarDecl(Id("name"),IntType)],[],[]),
+                FuncDecl(Id("foo"),[VarDecl(Id("a"),IntType),VarDecl(Id("a"),IntType)],[],[])])
+        expect = "Redeclared Parameter: a"
+        self.assertTrue(TestChecker.test(input,expect,404))
+
+    def test_type_not_coerc_int_float(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putInt"),[BinaryOp('+',IntLiteral(1), FloatLiteral(2.1))])])])
+        expect = "Type Mismatch In Statement: CallStmt(Id(putInt),[BinaryOp(+,IntLiteral(1),FloatLiteral(2.1))])"
+        self.assertTrue(TestChecker.test(input,expect,405))
+    
+    def test_type_coerc_int_float(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putFloat"),[BinaryOp('+',IntLiteral(1), IntLiteral(2))]),
+                                        CallStmt(Id("putInt"),[BinaryOp('+',FloatLiteral(1.3), IntLiteral(2))])])])
+        expect = "Type Mismatch In Statement: CallStmt(Id(putInt),[BinaryOp(+,FloatLiteral(1.3),IntLiteral(2))])"
+        self.assertTrue(TestChecker.test(input,expect,406))
+
+    def test_and_or_boolean(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putBoolLn"),[BinaryOp('and',BooleanLiteral(True), BooleanLiteral(False))]),
+                                        CallStmt(Id("putBool"),[BinaryOp('or',IntLiteral(1), FloatLiteral(2.1))])])])
+        expect = "Type Mismatch In Expression: BinaryOp(or,IntLiteral(1),FloatLiteral(2.1))"
+        self.assertTrue(TestChecker.test(input,expect,407))
+
+    def test_andthen_orelse_boolean(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putBoolLn"),[BinaryOp('andthen',BooleanLiteral(True), BooleanLiteral(False))]),
+                                        CallStmt(Id("putBool"),[BinaryOp('orelse',IntLiteral(1), FloatLiteral(2.1))])])])
+        expect = "Type Mismatch In Expression: BinaryOp(orelse,IntLiteral(1),FloatLiteral(2.1))"
+        self.assertTrue(TestChecker.test(input,expect,408))
+
+    def test_Unary_Type_sub(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putIntLn"),[UnaryOp('-', IntLiteral(1))]),
+                                        CallStmt(Id("putInt"),[UnaryOp('-',StringLiteral("string in unaryop"))])])])
+        expect = "Type Mismatch In Expression: UnaryOp(-,StringLiteral(string in unaryop))"
+        self.assertTrue(TestChecker.test(input,expect,409))
+
+    def test_Unary_Type_Not(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putBoolLn"),[UnaryOp('not', BooleanLiteral(True))]),
+                                        CallStmt(Id("putBool"),[UnaryOp('not',StringLiteral("string in unaryop"))])])])
+        expect = "Type Mismatch In Expression: UnaryOp(not,StringLiteral(string in unaryop))"
+        self.assertTrue(TestChecker.test(input,expect,410))
+
+    def test_Unary_Type_Not(self):
+        """Simple program: int main() {} """
+        input = Program([
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putBoolLn"),[UnaryOp('not', BinaryOp('>',IntLiteral(1),IntLiteral(2)))]),
+                                        CallStmt(Id("putBool"),[UnaryOp('not',BinaryOp('+', IntLiteral(10000), IntLiteral(0)))])])])
+        expect = "Type Mismatch In Expression: UnaryOp(not,BinaryOp(+,IntLiteral(10000),IntLiteral(0)))"
+        self.assertTrue(TestChecker.test(input,expect,411))'''
+    def test_Undeclared_id(self):
+        """Simple program: int main() {} """
+        input = Program([
+                VarDecl(Id("a"),IntType),
+                FuncDecl(Id("foo"),[],[],[CallStmt(Id("putBoolLn"),[BinaryOp('>',Id("a"),IntLiteral(2))]),
+                                        CallStmt(Id("putBool"),[BinaryOp('>',Id("b"),IntLiteral(2))])])])
+        expect = "Type Mismatch In Expression: UnaryOp(not,BinaryOp(+,IntLiteral(10000),IntLiteral(0)))"
+        self.assertTrue(TestChecker.test(input,expect,412))
+
+    def Atest_diff_numofparam_stmt(self):
         """More complex program"""
         input = """procedure main (); begin
             putIntLn();
@@ -33,14 +112,14 @@ class CheckerSuite(unittest.TestCase):
         expect = "Type Mismatch In Statement: CallStmt(Id(putIntLn),[])"
         self.assertTrue(TestChecker.test(input,expect,404))
 
-    def test_undeclared_function_use_ast(self):
+    def Atest_undeclared_function_use_ast(self):
         """Simple program: int main() {} """
         input = Program([FuncDecl(Id("main"),[],[],[
             CallStmt(Id("foo"),[])])])
         expect = "Undeclared Procedure: foo"
         self.assertTrue(TestChecker.test(input,expect,420))
 
-    def test_diff_numofparam_expr_use_ast(self):
+    def Atest_diff_numofparam_expr_use_ast(self):
         """More complex program"""
         input = Program([
                 FuncDecl(Id("main"),[],[],[
